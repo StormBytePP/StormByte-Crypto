e74d4986baf913529c352f8b357784e50a81bd0d
David C. Manuelda <StormByte@gmail.com>
2026-09-04 22:32:06 +0200

    docs: symmetric crypter base + AES

--- lib/public/StormByte/crypto/crypter/symmetric/aes.hxx
+++ lib/public/StormByte/crypto/crypter/symmetric/aes.hxx
@@ -1,86 +1,90 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte-Crypto.
- *
- * StormByte-Crypto is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte-Crypto is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte-Crypto. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte-Crypto.
+*
+* StormByte-Crypto is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte-Crypto is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte-Crypto. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
 #include <StormByte/crypto/crypter/symmetric/generic.hxx>
 
 /**
- * @namespace Crypter
- * @brief The namespace containing all the crypter-related classes.
+ * @brief Ciphers of the Crypto module.
  */
 namespace StormByte::Crypto::Crypter {
 	/**
 	 * @class AES
-	 * @brief A symmetric crypter class.
+	 * @brief AES-CBC crypter.
 	 */
 	class STORMBYTE_CRYPTO_PUBLIC AES final: public Symmetric {
 		public:
 			/**
-			 * @brief Constructor
-			 * @param password The password used for encryption/decryption.
+			 * @name Construction
+			 * @{
+			 */
+			/**
+			 * @brief Construct with a password.
+			 * @param password Password.
 			 */
 			inline AES(class Password password):
 				Symmetric(Type::AES, std::move(password)) {}
 
 			/**
-			 * @brief Copy constructor
-			 * @param other The other AES crypter to copy from.
+			 * @brief Copy constructor.
+			 * @param other Crypter to copy.
 			 */
 			AES(const AES& other) = default;
 
 			/**
-			 * @brief Move constructor
-			 * @param other The other AES crypter to move from.
+			 * @brief Move constructor.
+			 * @param other Crypter to move.
 			 */
 			AES(AES&& other) noexcept = default;
 
 			/**
-			 * @brief Virtual destructor
+			 * @brief Destructor.
 			 */
 			virtual ~AES() noexcept = default;
 
 			/**
-			 * @brief Copy assignment operator
-			 * @param other The other AES crypter to copy from.
-			 * @return Reference to this AES crypter.
+			 * @brief Copy assignment.
+			 * @param other Crypter to copy.
+			 * @return Reference to this crypter.
 			 */
 			AES& operator=(const AES& other) = default;
 
 			/**
-			 * @brief Move assignment operator
-			 * @param other The other AES crypter to move from.
-			 * @return Reference to this AES crypter.
+			 * @brief Move assignment.
+			 * @param other Crypter to move.
+			 * @return Reference to this crypter.
 			 */
 			AES& operator=(AES&& other) noexcept = default;
+			/** @} */
 
 			/**
-			 * @brief Clone the AES crypter.
-			 * @return A pointer to the cloned AES crypter.
+			 * @brief Clone this crypter.
+			 * @return Shared pointer to the clone.
 			 */
 			inline PointerType Clone() const noexcept override {
 				return std::make_shared<AES>(*this);
 			}
 
 			/**
-			 * @brief Move the AES crypter.
-			 * @return A pointer to the moved AES crypter.
+			 * @brief Move this crypter into a new instance.
+			 * @return Shared pointer to the moved crypter.
 			 */
 			inline PointerType Move() noexcept override {
 				return std::make_shared<AES>(std::move(*this));
@@ -88,34 +92,34 @@ namespace StormByte::Crypto::Crypter {
 
 		private:
 			/**
-			 * @brief Implementation of the encryption logic.
-			 * @param input The input buffer to encrypt.
-			 * @param output The output buffer to write the encrypted data to.
-			 * @return true if encryption was successful, false otherwise.
+			 * @brief Encrypt a byte span.
+			 * @param input Input bytes.
+			 * @param output Destination buffer.
+			 * @return true on success.
 			 */
 			bool DoEncrypt(std::span<const std::byte> input, Buffer::WriteOnly& output) const noexcept override;
 
 			/**
-			 * @brief Implementation of the encryption logic for Consumer buffers.
-			 * @param consumer The Consumer buffer to encrypt.
-			 * @param mode The read mode indicating copy or move.
-			 * @return A Consumer buffer containing the encrypted data.
+			 * @brief Encrypt a Consumer.
+			 * @param consumer Input consumer.
+			 * @param mode Copy or move.
+			 * @return Consumer with ciphertext.
 			 */
 			Buffer::Consumer DoEncrypt(Buffer::Consumer consumer, ReadMode mode) const noexcept override;
 
 			/**
-			 * @brief Implementation of the decryption logic.
-			 * @param input The input buffer to decrypt.
-			 * @param output The output buffer to write the decrypted data to.
-			 * @return true if decryption was successful, false otherwise.
+			 * @brief Decrypt a byte span.
+			 * @param input Input bytes.
+			 * @param output Destination buffer.
+			 * @return true on success.
 			 */
 			bool DoDecrypt(std::span<const std::byte> input, Buffer::WriteOnly& output) const noexcept override;
 
 			/**
-			 * @brief Implementation of the decryption logic for Consumer buffers.
-			 * @param consumer The Consumer buffer to decrypt.
-			 * @param mode The read mode indicating copy or move.
-			 * @return A Consumer buffer containing the decrypted data.
+			 * @brief Decrypt a Consumer.
+			 * @param consumer Input consumer.
+			 * @param mode Copy or move.
+			 * @return Consumer with plaintext.
 			 */
 			Buffer::Consumer DoDecrypt(Buffer::Consumer consumer, ReadMode mode) const noexcept override;
 	};
--- lib/public/StormByte/crypto/crypter/symmetric/generic.hxx
+++ lib/public/StormByte/crypto/crypter/symmetric/generic.hxx
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte-Crypto.
- *
- * StormByte-Crypto is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte-Crypto is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte-Crypto. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte-Crypto.
+*
+* StormByte-Crypto is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte-Crypto is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte-Crypto. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
@@ -23,84 +23,87 @@
 #include <StormByte/crypto/password.hxx>
 
 /**
- * @namespace Crypter
- * @brief The namespace containing all the crypter-related classes.
+ * @brief Ciphers of the Crypto module.
  */
 namespace StormByte::Crypto::Crypter {
 	/**
 	 * @class Symmetric
-	 * @brief A generic symmetric crypter class.
+	 * @brief Password-based symmetric crypter.
 	 *
-	 * Stores the encryption password as a @ref StormByte::Crypto::Password
-	 * so that all copies share the same underlying buffer and the memory is
-	 * securely wiped when the last owner is destroyed.
+	 * Copies share the same @ref StormByte::Crypto::Password. The buffer is
+	 * wiped when the last owner is destroyed.
 	 */
 	class STORMBYTE_CRYPTO_PUBLIC Symmetric: public Generic {
 		public:
 			/**
-			 * @brief Copy constructor
-			 * @param other The other Symmetric crypter to copy from.
+			 * @name Construction
+			 * @{
+			 */
+			/**
+			 * @brief Copy constructor.
+			 * @param other Crypter to copy.
 			 */
 			Symmetric(const Symmetric& other) = default;
 
 			/**
-			 * @brief Move constructor
-			 * @param other The other Symmetric crypter to move from.
+			 * @brief Move constructor.
+			 * @param other Crypter to move.
 			 */
 			Symmetric(Symmetric&& other) noexcept = default;
 
 			/**
-			 * @brief Virtual destructor
+			 * @brief Destructor.
 			 */
 			virtual ~Symmetric() noexcept override;
 
 			/**
-			 * @brief Copy assignment operator
-			 * @param other The other Symmetric crypter to copy from.
-			 * @return Reference to this Symmetric crypter.
+			 * @brief Copy assignment.
+			 * @param other Crypter to copy.
+			 * @return Reference to this crypter.
 			 */
 			Symmetric& operator=(const Symmetric& other) = default;
 
 			/**
-			 * @brief Move assignment operator
-			 * @param other The other Symmetric crypter to move from.
-			 * @return Reference to this Symmetric crypter.
+			 * @brief Move assignment.
+			 * @param other Crypter to move.
+			 * @return Reference to this crypter.
 			 */
 			Symmetric& operator=(Symmetric&& other) noexcept = default;
+			/** @} */
 
 			/**
-			 * @brief Gets the password used for symmetric encryption.
-			 * @return A const reference to the stored @ref Password.
-			 * @note The returned object shares ownership of the underlying data.
+			 * @brief Password used by this crypter.
+			 * @return Shared password.
+			 * @note Shares ownership of the underlying bytes.
 			 */
 			inline const class Password& Password() const noexcept {
 				return m_password;
 			}
 
 			/**
-			 * @brief Generates a random password for symmetric encryption.
-			 * @param length Number of random bytes to generate.
-			 * @return A @ref Password containing raw random bytes (not text/hex).
+			 * @brief Random password of raw bytes (not text).
+			 * @param length Number of bytes.
+			 * @return Password.
 			 */
 			static class Password RandomPassword(size_t length = 32) noexcept;
 
 		protected:
-			class Password m_password;	///< The password used for symmetric encryption
+			class Password m_password;	///< Shared password
 
 			/**
-			 * @brief Constructor
-			 * @param type The type of crypter.
-			 * @param password The password to use for encryption/decryption.
+			 * @brief Construct with a cipher and a password.
+			 * @param type Cipher.
+			 * @param password Password.
 			 */
 			inline Symmetric(enum Type type, class Password password):
 				Generic(type), m_password(std::move(password)) {}
 	};
 
 	/**
-	 * @brief Creates a symmetric crypter based on the type.
-	 * @param type The type of crypter.
-	 * @param password The password to use for the crypter.
-	 * @return A pointer to the created symmetric crypter, or nullptr on failure.
+	 * @brief Factory for a symmetric crypter.
+	 * @param type Cipher.
+	 * @param password Password.
+	 * @return Crypter pointer, or nullptr on failure.
 	 */
 	STORMBYTE_CRYPTO_PUBLIC Generic::PointerType Create(enum Type type, class Password password) noexcept;
 }
